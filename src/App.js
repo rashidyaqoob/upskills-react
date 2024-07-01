7c844a84f9c8e97166882a260900a332be23054c
Tehreem Fazili <tehreemfazili5@gmail.com>
2024-07-01 15:49:03 +0530

    USR-incomeexpense: Moved folder to components, added class to in app.jsx

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import "./App.css";
 import TransactionList from "./components/Transaction/TransactionList";
-import Balance from "./Balance/Balance";
-import Header from "./Header/Header";
-import IncomeExpense from "./IncomeExpense/IncomeExpense";
+import Balance from "./components/Balance/Balance";
+import Header from "./components/Header/Header";
+import IncomeExpense from "./components/IncomeExpense/IncomeExpense";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Balance />
-      <IncomeExpense />
-      <TransactionList />
+      <div className="container">
+        <Balance />
+        <IncomeExpense />
+        <TransactionList />
+      </div>
     </div>
   );
 }
