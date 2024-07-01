922546ad228fc3ce54cbcbfea5e50b10a17c4a29
Rashid <rashid.yaqoob@axelerant.com>
2024-07-01 15:40:24 +0530

    USR-transaction: Make folders

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import "./App.css";
+import TransactionList from "./components/Transaction/TransactionList";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <TransactionList />
     </div>
   );
 }
